21b3afde9f71dedebae228e78aa729989dcae7a4
Abylay <auzakbaev20@gmail.com>
2020-09-29 11:23:07 +0500

    My solution Exercise Archive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+import { ArchivePageComponent } from './archive-page/archive-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ArchivePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'archive/:year/:month', component: ArchivePageComponent},
+      { path: '**', component: NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/archive-page/archive-page.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-archive-page',
+  templateUrl: './archive-page.component.html',
+  styleUrls: ['./archive-page.component.css'],
+})
+export class ArchivePageComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  yearArchive: number;
+  monthArchive: number;
+  
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      var year = +params.get('year');
+      var month = +params.get('month');
+      
+      this.yearArchive = year;
+      this.monthArchive = month;
+
+    });
+  }
+
+  viewAll(){
+    this.router.navigate([''])
+  }
+
+
+}
